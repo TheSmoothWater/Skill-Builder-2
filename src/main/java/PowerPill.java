745a67a66e1c470b7870879d1d07b0948acfa039
TheSmoothWater <brejonturner@gmail.com>
2024-02-11 14:59:31 -0500

    Done and Graded

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -9,30 +9,60 @@
  */
 public class PowerPill
 {
-    // TODO - replace this line with instruction from step 1
+    static int DEFAULT_POWER = 10;
+    int power;
+    String name;
 
-    // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    /**
+     \* Initializes this power pill to a default power value
+     \* and sets the name of the pill to name.
+     \* @param name the name of this power pill.
+     */
+    public PowerPill(String name){
+        this.name = name;
+        power = DEFAULT_POWER;
+    }
 
-    // constructors
+    public PowerPill(String name, int power){
+        this.name = name;
+        this.power = power;
+    }
 
-    // TODO - replace this line with instruction from step 4
+    /**
+     * Gets name of PowerPill object
+     * @return name
+     */
+    public String getName(){
+        return name;
+    }
 
-    // TODO - replace this line with instruction from step 5
+    /**
+     * Gets power of PowerPill object
+     * @return power
+     */
+    public int getPower(){
+        return power;
+    }
+
+    /**
+     * Sets name of PowerPill Object
+     * @param name
+     */
+    public void setName(String name){this.name = name;}
+
+    /**
+     * Sets power of PowerPill Object
+     * @param power
+     */
+    public void setPower(int power){this.power = power;}
 
 
-    // accessor methods
-
-    // TODO - replace this line with instruction from step 6
-
-
-    // mutator methods
-
-    // TODO - replace this line with instruction from step 7
-
-    // toString method
-
-    // TODO - replace this line with instruction from step 8
+    /**
+     * Returns string of PowerPill object
+     * @return toString
+     */
+    public String toString(){
+        return "PowerPill " + name + " = " + power;
+    }
 
 }
